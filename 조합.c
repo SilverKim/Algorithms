d56226de61451756f133997b8c78b14ceab0f45b
SilverKim <33801778+SilverKim@users.noreply.github.com>
2019-03-16 18:00:33 +0100

    Combination

---
+++ 조합.c
@@ -0,0 +1,17 @@
+ #include <stdio.h>
+
+int main()
+{
+	int i,x;
+	int arr[4][9];
+
+	for (i = 0; i < 36; i++) {
+		scanf("%d", &arr[i]);
+
+		if ((char*)arr[i] < (char*)arr[i + 1]) {
+			
+		}
+
+
+	}
+} 
